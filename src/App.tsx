2a173fd864a4a0c2351d66b9709a8807f39db497
Guillaume Gomez <guillaume.gomez846@gmail.com>
2023-03-31 09:18:44 +0200

    clean up and start to work on algorithm to build an image based on threshold

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,8 @@ const possibleColors = ["#000000", "#FF0000", "#00FF00", "#0000FF", "#FFFFFF"];
 function App() {
   const [numberOfLayers, setNumberOfLayer] = useState<number>(2);
   const [layers, setLayers] = useState<HTMLCanvasElement[]>([]);
-  const imageRef = useRef<HTMLImageElement>();
-  const canvasRef = useRef<HTMLCanvasElement>();
+  const imageRef = useRef<HTMLImageElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
   function generateImagesFromLayers() {
 
--- src/tools.ts
+++ src/tools.ts
@@ -11,7 +11,6 @@ export function imageToGrayScaleCanvas(image : HTMLImageElement, canvas: HTMLCan
 
 }
 
-
 function convertToGrayScale(context: CanvasRenderingContext2D, width: number, height: number) {
   const imageData = context.getImageData(0, 0, width, height);
   for (let i = 0; i < imageData.data.length; i += 4) {
@@ -27,4 +26,50 @@ function convertToGrayScale(context: CanvasRenderingContext2D, width: number, he
    imageData.data[i + 3] = 255;
   }
   context.putImageData(imageData, 0, 0);
+}
+
+function generateImageFromRange(greyScaleCanvas: HTMLCanvasElement, min: number, max: number) {
+  const canvas = document.createElement("canvas");
+  canvas.width = greyScaleCanvas.width;
+  canvas.height = greyScaleCanvas.height;
+
+  const greyScaleContext = greyScaleCanvas.getContext("2d");
+  const outputContext = canvas.getContext("2d");
+
+  if(greyScaleContext && outputContext) {
+    copyGreyCanvasByRange(greyScaleContext, outputContext, canvas.width, canvas.height, min, max);
+  }
+
+  throw new Error("Cannot find the greyScaleContext or outputContext");
+
+
+}
+
+function copyGreyCanvasByRange(
+  greyScaleContext: CanvasRenderingContext2D,
+  outputContext: CanvasRenderingContext2D,
+  width: number,
+  height: number,
+  min: number,
+  max: number
+  ) {
+  const imageData = greyScaleContext.getImageData(0, 0, width, height);
+  const imageDateOutput = outputContext.getImageData(0, 0, width, height);
+
+  for (let i = 0; i < imageData.data.length; i += 4) {
+    // as gray image, all components are the same
+    const value = imageData.data[i];
+    if(min <= value && value >= max) {
+      imageDateOutput.data[i] = value;
+      imageDateOutput.data[i + 1] = value;
+      imageDateOutput.data[i + 2] = value;
+      imageDateOutput.data[i + 3] = 255;
+    } else {
+      imageDateOutput.data[i] = 0;
+      imageDateOutput.data[i + 1] = 0;
+      imageDateOutput.data[i + 2] = 0;
+      imageDateOutput.data[i + 3] = 255;
+    }
+  }
+  outputContext.putImageData(imageDateOutput, 0, 0);
 }

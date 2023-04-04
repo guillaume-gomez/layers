cc17d42c3cf79557e1b9b1be58650e84d91cacb6
Guillaume Gomez <guillaume.gomez846@gmail.com>
2023-04-04 16:25:01 +0200

    add a helper component to pick a color

---
+++ src/Components/ColorPicker.tsx
@@ -0,0 +1,99 @@
+import React, { useState } from 'react';
+import ClickAwayListener from 'react-click-away-listener';
+
+const COLORS = [
+  "#000000",
+  "#914E72",
+  "#0078BF",
+  "#00A95C",
+  "#3255A4",
+  "#F15060",
+  "#3D5588",
+  "#765BA7",
+  "#00838A",
+  "#BB8B41",
+  "#407060",
+  "#FF665E",
+  "#925F52",
+  "#FFE800",
+  "#D2515E",
+  "#FF6C2F",
+  "#FF48B0",
+  "#88898A"
+];
+
+//more colors https://www.stencil.wiki/colors
+
+const IN_BLACK :string[] = ["#FFE800", "#00A95C" ];
+
+interface ColorPickerInterface {
+  label: string;
+  initialColorClass?: string;
+  onChange: (value: string) => void;
+}
+
+function ColorPicker({label, initialColorClass = COLORS[1], onChange} : ColorPickerInterface) {
+  const [currentColor, setCurrentColor] = useState<string>(initialColorClass);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [iconColorClass, setIconColorClass] = useState<string>("text-white");
+
+  function selectColor(color: string) {
+    setCurrentColor(color);
+    onChange(color);
+    if (IN_BLACK.includes(color)) {
+      setIconColorClass("text-black");
+    }
+    else {
+      setIconColorClass("text-white");
+    }
+  }
+
+  function handleClickAway() {
+    setIsOpen(!isOpen);
+  }
+
+  return (
+    <div>
+      <div>
+        <label className="block mb-1 text-base font-semibold">{label}</label>
+        <div className="flex flex-row relative">
+          <input id="color-picker" disabled className="w-full border border-gray-400 p-2 rounded-lg text-white" value={currentColor} />
+          <div
+            onClick={()=> setIsOpen(!isOpen)}
+            style={{background: currentColor }}
+            className={`cursor-pointer rounded-full ml-3 my-auto h-10 w-10 flex`}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg"className={`${iconColorClass} h-6 w-6 mx-auto my-auto`} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01" />
+            </svg>
+          </div>
+          {isOpen ?
+            <ClickAwayListener onClickAway={handleClickAway}>
+            <div className="overflow-y-scroll h-36 border-2 border-gray-300 origin-top-right absolute right-0 top-full mt-2 rounded-md shadow-lg">
+              <div className="rounded-md bg-base-200 shadow-xs p-2">
+                <div className="grid grid-flow-row-dense grid-cols-5">
+                  {
+                    COLORS.map(color => 
+                      (
+                        <div
+                          style={{background: color}}
+                          onClick={()=> selectColor(color)}
+                          className={`cursor-pointer w-6 h-6 rounded-full mx-1 my-1`}>
+                        </div>
+                      )
+                    )
+                  }
+                </div>
+              </div>
+            </div>
+            </ClickAwayListener>
+            : null
+          }
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ColorPicker;

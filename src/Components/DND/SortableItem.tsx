0610fefe591a13fee95ae52ea045d68f0e7429c1
Guillaume Gomez <guillaume.gomez846@gmail.com>
2023-04-26 18:21:09 +0200

    change cursor

--- src/Components/DND/SortableItem.tsx
+++ src/Components/DND/SortableItem.tsx
@@ -63,7 +63,7 @@ export function DragHandle() {
   const { attributes, listeners, ref } = useContext(SortableItemContext);
 
   return (
-    <button className="btn btn-square btn-secondary focus-visible:shadow-md" {...attributes} {...listeners} ref={ref}>
+    <button className="cursor-grabbing btn btn-square btn-secondary focus-visible:shadow-md" {...attributes} {...listeners} ref={ref}>
       <svg viewBox="0 0 20 20" width="24">
         <path d="M7 2a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 2zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 8zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 14zm6-8a2 2 0 1 0-.001-4.001A2 2 0 0 0 13 6zm0 2a2 2 0 1 0 .001 4.001A2 2 0 0 0 13 8zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 13 14z"></path>
       </svg>

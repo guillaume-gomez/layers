aa437768a7d8b42e4e71242d141ca1371da716cb
Guillaume Gomez <guillaume.gomez846@gmail.com>
2023-05-26 20:33:27 +0200

    use tabs for layer setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useRef, useEffect } from 'react';
 import { times, uniqueId } from "lodash";
-import { useOnWindowResize } from "rooks";
+import { useWindowSize } from "rooks";
 import { imageToGrayScaleCanvas, generateImageFromRange } from "./tools";
 import Slider from "./Components/Slider";
 import ColorPicker from "./Components/ColorPicker";
@@ -35,13 +35,13 @@ function App() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const resultRef = useRef<HTMLDivElement>(null);
 
+  const { innerWidth, innerHeight } = useWindowSize();
+
+
   useEffect(() => {
     limitSize();
-  }, [])
+  }, [innerHeight, innerWidth])
 
-  useOnWindowResize(() => {
-    limitSize();
-  });
 
   useEffect(() => {
     generateImagesFromLayers();
@@ -51,7 +51,7 @@ function App() {
     const newWidth = (resultDivRef.current as any).clientWidth;
     const newHeight = (resultDivRef.current as any).clientHeight;
 
-    const newPredefinedWidth = newWidth - 50;
+    const newPredefinedWidth = newWidth - 100;
     setWidth(newPredefinedWidth);
     setHeight(newPredefinedWidth * 9/16);
 
@@ -140,8 +140,8 @@ function App() {
       <div className="w-4/5 hidden">
         <RangeSlider min={1} max={10000}/>
       </div>
-      <div className="container flex xl:flex-row flex-col gap-3">
-        <div className="settings card bg-base-200">
+      <div className="w-full flex md:flex-row flex-col gap-3 md:p-5 p-2">
+        <div className="settings card bg-base-200" style={{ minWidth: 325 }}>
           <div className="card-body p-2">
             <div className="card-title">Settings</div>
             <CollapsibleCardManager>
@@ -169,7 +169,7 @@ function App() {
             </CollapsibleCardManager>
           </div>
         </div>
-        <div className="render card bg-base-200 basis-10/12" ref={resultDivRef}>
+        <div className="render card bg-base-200 w-full" ref={resultDivRef}>
           <div className="card-body p-2">
             <div className="card-title">Renderer</div>
             <img ref={imageRef} className="hidden"/>
--- src/Components/LayerSettings.tsx
+++ src/Components/LayerSettings.tsx
@@ -9,7 +9,11 @@ interface LayerSettingsInterface {
   layerSettings: LayerSettingsData;
 }
 
+const TabNames = ["color", "range", "position"];
+type TabName = typeof TabNames[number];
+
 function LayerSettings({ onChange, layerSettings } : LayerSettingsInterface) {
+  const [selectedTab, setSelectedTab] = useState<TabName>("color");
 
   function handleChangeMin(value: number) {
     onChange({ ...layerSettings, min: value });
@@ -39,55 +43,94 @@ function LayerSettings({ onChange, layerSettings } : LayerSettingsInterface) {
     onChange({ ...layerSettings, position2D:{ ...layerSettings.position2D, y: value }});
   }
 
+  function renderTab() {
+    switch(selectedTab) {
+      case "color":
+      default:
+        return (
+          <>
+            <ColorPicker label="Color" value={layerSettings.color} onChange={(color) => handleChangeColor(color)}/>
+            <Slider
+              label="Alpha"
+              onChange={(value) => handleChangeAlpha(value)}
+              value={layerSettings.alpha}
+              min={0}
+              max={255}
+            />
+          </>
+        );
+      case "range":
+        return(
+          <>
+            <Slider
+            label="Min"
+            onChange={(value) => handleChangeMin(value)}
+            value={layerSettings.min}
+            min={0}
+            max={255}
+          />
+          <Slider
+            label="Max"
+            onChange={(value) => handleChangeMax(value)}
+            value={layerSettings.max}
+            min={0}
+            max={255}
+          />
+          <Slider
+              label="Noise"
+              onChange={(value) => handleChangePercentage(value)}
+              value={layerSettings.noise}
+              min={0}
+              max={100}
+            />
+          </>
+        );
+      case "position":
+        return(
+          <>
+            <Slider
+            label="X axis"
+            onChange={(value) => handleChangeX(value)}
+            value={layerSettings.position2D.x}
+            min={-1}
+            max={1}
+            float
+            step={0.001}
+          />
+          <Slider
+            label="Y Axis"
+            onChange={(value) => handleChangeY(value)}
+            value={layerSettings.position2D.y}
+            min={-1}
+            max={1}
+            float
+            step={0.001}
+          />
+          </>
+        );
+    };
+
+  }
+
   return (
-      <div className="flex flex-col gap-3">
-        <Slider
-          label="Min"
-          onChange={(value) => handleChangeMin(value)}
-          value={layerSettings.min}
-          min={0}
-          max={255}
-        />
-        <Slider
-          label="Max"
-          onChange={(value) => handleChangeMax(value)}
-          value={layerSettings.max}
-          min={0}
-          max={255}
-        />
-        <ColorPicker label="Color" value={layerSettings.color} onChange={(color) => handleChangeColor(color)}/>
-        <Slider
-          label="Noise"
-          onChange={(value) => handleChangePercentage(value)}
-          value={layerSettings.noise}
-          min={0}
-          max={100}
-        />
-        <Slider
-          label="Alpha"
-          onChange={(value) => handleChangeAlpha(value)}
-          value={layerSettings.alpha}
-          min={0}
-          max={255}
-        />
-        <Slider
-          label="position X"
-          onChange={(value) => handleChangeX(value)}
-          value={layerSettings.position2D.x}
-          min={-1}
-          max={1}
-          float
-          step={0.001}
-        />
-        <Slider
-          label="position Y"
-          onChange={(value) => handleChangeY(value)}
-          value={layerSettings.position2D.y}
-          min={-1}
-          max={1}
-          float
-          step={0.001}
-        />
+      <div className="flex flex-col gap-4">
+        <div className="tabs tabs-boxed">
+          {
+            TabNames.map(tabName => {
+            return(
+              <a
+                onClick={() => setSelectedTab(tabName)}
+                className={`tab tab-sm ${selectedTab === tabName ? "tab-active" :"" }`}
+              >
+                {tabName}
+              </a>
+            )
+          })
+          }
+        </div>
+        <div className="flex flex-col gap-3">
+         {renderTab()}
+        </div>
       </div>
   )
 }

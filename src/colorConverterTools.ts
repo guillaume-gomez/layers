f7ed2aef32661ba8822d68e5253ecd654138e1ba
Guillaume Gomez <guillaume.gomez846@gmail.com>
2023-06-24 13:38:19 +0200

    add a new tool

--- src/colorConverterTools.ts
+++ src/colorConverterTools.ts
@@ -1,3 +1,5 @@
+import { RGBArray } from "./interfaces";
+
 export function hashCode(hexString: string) : number {
     let hash = 0;
     for (let i = 0; i < hexString.length; i++) {
@@ -12,4 +14,19 @@ export function intToRGB(intColor: number): string {
         .toUpperCase();
 
     return "00000".substring(0, 6 - color.length) + color;
+}
+
+export function hexToRGB(hexColor: string, alpha: number) : RGBArray {
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hexColor);
+
+    function hexToDec(hex: string) {
+      return parseInt(hex, 16);
+    }
+
+    return [
+      hexToDec(result![1]),
+      hexToDec(result![2]),
+      hexToDec(result![3]),
+      alpha
+    ];
 }

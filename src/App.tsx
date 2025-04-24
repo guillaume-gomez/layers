f8e5583c3f2a9be11d0cd393311b2cf4d03731a3
guillaume-gomez <guillaume.gomez846@gmail.com>
2025-04-24 12:04:34 +0200

    remove console.log + usage of test component

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,9 @@ import './App.css'
 function App() {
   return (
     <div className="flex flex-col justify-center items-center md:p-5 p-2 gap-3">
-      <div className="w-4/5 hidden">
-        <RangeSlider min={1} max={10000}/>
-      </div>
-      <Header/>
       <LayersSettingsProvider>
         <MainContent />
       </LayersSettingsProvider>
-      <Footer />
     </div>
   )
 }
